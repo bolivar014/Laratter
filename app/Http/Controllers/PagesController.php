0f6a06659abbad6fa7b217cbdbb78301b8398c65
unknown <bolivar014@gmail.com>
2019-06-24 16:37:47 -0500

    Integrando Bootstrap en el Proyecto

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -9,15 +9,32 @@ class PagesController extends Controller
     // 
     public function home()
     {
-        $links = [
-            'https://platzi.com/laravel' => 'Curso De Laravel',
-            'https://laravel.com' => 'Pagina Oficial De Laravel'
+        $messages = [
+            [     
+                'id' => 1,
+                'content' => 'Este es un mensaje de prueba',
+                'image' => 'http://lorempixel.com/600/338?1',
+            ],
+            [     
+                'id' => 2,
+                'content' => 'Este es otro mensaje de prueba',
+                'image' => 'http://lorempixel.com/600/338?2',
+            ],
+            [     
+                'id' => 3,
+                'content' => 'Este es un mensaje de prueba nuevo',
+                'image' => 'http://lorempixel.com/600/338?3',
+            ],
+            [     
+                'id' => 4,
+                'content' => 'Este es un mensaje de prueba',
+                'image' => 'http://lorempixel.com/600/338?4',
+            ],
         ];
-    
+
         return view('welcome',[
-            'user' => 'Juan Camilo Valdes Bolivar',
-            'links' => $links,
-        ]);
+            'messages' => $messages,
+            ]);
     }
 
     public function about()
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,19 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="title m-b-md">
-        Laratter
+    <div class="jumbotron text-center">
+        <h1>Laratter</h1>
+        <nav>
+            <ul class="nav nav-pills">
+                <li class="nav-item">
+                    <a href="/" class="nav-link">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a href="/about" class="nav-link">Acerca</a>
+                </li>
+                <li class="nav-item">
+                    <a href="/" class="nav-link">Pruebas</a>
+                </li>
+            </ul>
+        </nav>
     </div>
 
-    @if(isset($user))
-        <p>Usuario: {{ $user }} </p>
-    @else
-        <p>Usuario Pendiente Por Definir</p>
-    @endif
-        
-    <div class="links">
-        @foreach($links as $link => $text)
-            <a href="{{ $link }}"> {{ $text }} </a>
-        @endforeach
+    <div class="row">
+        @forelse($messages as $message)
+            <div class="col-6">
+                <img src="{{ $message['image'] }}" alt="" class="img-thumbnail">
+                <p class="card-text">
+                    {{ $message['content'] }}
+                    <a href="/messages/{{ $message['id']}}">Leer Más...</a>
+                </p>
+            </div>
+        @empty
+            <p>No Hay Mensajes Destacados...</p>
+        @endforelse
     </div>
 @endsection
