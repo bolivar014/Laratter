3f89a7f7e6f4229e8760ea1215eebab63699a847
unknown <bolivar014@gmail.com>
2019-06-27 18:30:44 -0500

    Creación de seeds para agregar registros en la db.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,3 +25,19 @@ $factory->define(User::class, function (Faker $faker) {
         'remember_token' => Str::random(10),
     ];
 });
+/*
+    TIPOS DE DATOS EN FAKER...
+    $faker->word // genera una sola palabra.
+    $faker->word(5,true) // Genera un texto de 5 palabras, debe ir en modo TRUE.
+    $faker->paragraph // Genera un parrafo.
+    $faker->realText() // Genera texto de libros.
+
+*/
+
+//Función Faker, nos permite creación de datos 
+$factory->define(App\Message::class, function(Faker $faker){
+    return [
+        'content' => $faker->realText(random_int(20, 160)), // Se le asigna una longitud en el texto de 20 a 160 caracteres.
+        'image' => $faker->imageUrl(600,338)
+    ];
+});
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        // Creamos 100 Mensajes nuevos en la tabla Message
+        factory(App\Message::class)
+		->times(100) // Generamos 100 mensajes y los creamos
+		->create();
     }
 }
